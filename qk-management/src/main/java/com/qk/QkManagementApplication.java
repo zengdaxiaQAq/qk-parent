e1ab846fd77c6c9f0339a7dc36485671dc3cdefc
zengdaxiaQAq <18906317206@163.com>
2026-04-20 20:28:27 +0800

    添加到本地

--- qk-management/src/main/java/com/qk/QkManagementApplication.java
+++ qk-management/src/main/java/com/qk/QkManagementApplication.java
@@ -3,11 +3,13 @@ package com.qk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class QkManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QkManagementApplication.class, args);
 	}
-
+	//dasjdlksajkldjasdlfasdadasds
 }
